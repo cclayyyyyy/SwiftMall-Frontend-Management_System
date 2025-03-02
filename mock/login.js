65de1e94fbecec35672b9f8100e09be0c404b2cd
Hongyu Chen <130239564+cclayyyyyy@users.noreply.github.com>
2025-03-02 12:55:24 -0500

    Update login.js

--- mock/login.js
+++ mock/login.js
@@ -5,14 +5,14 @@ export default [
     timeout: 1000,
     statusCode: 200,
     response: ({ body }) => {
-      // 响应内容
+      
       return +body.password === 123456
         ? {
             code: 200,
             message: '登录成功',
             data: {
-              token: '@word(50, 100)', // @word()是mockjs的语法
-              refresh_token: '@word(50, 100)', // refresh_token是用来重新生成token的
+              token: '@word(50, 100)', 
+              refresh_token: '@word(50, 100)', 
             },
           }
         : {
